f0f15c9da5872b235a4d6e5fd730d272c0cb7baf
Raju Melveetilpurayil <codename.raju@gmail.com>
2021-09-18 05:02:19 +0100

    [~] Food App till meal form

--- section11-food-ordering-app/src/App.js
+++ section11-food-ordering-app/src/App.js
@@ -1,19 +1,17 @@
 
+import Header  from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 import './App.css';
-import {useState} from 'react';
-import Button from './components/UI/Button/Button';
 
 function App() {
-const [showParagraph, setShowParagraph] = useState(false)  
-const toggleParagraphHandler=()=>{
-  setShowParagraph((prevParagraph)=>!prevParagraph);
-}
+
   return (
-    <div className="app">
-      <h1>Hello {Math.random()}</h1>
-      {showParagraph&&<p>This is new!</p>}
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
-    </div>
+    <>
+      <Header></Header>
+      <main>
+        <Meals/>
+      </main>
+    </>
   );
 }
 
